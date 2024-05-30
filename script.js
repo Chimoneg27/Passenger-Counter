b0d29f954bef6d179ed3700f9d07d071998f70ff
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-05-30 15:08:22 +0200

    Add save function

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-let count = 0;
-let countEl = document.getElementById("count-el");
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+let count = 0
 
-const increment = () => {
-  count = count + 1;
-  countEl.innerText = count;
-  console.log(count);
+function increment() {
+  count += 1
+  countEl.innerText = count
+}
+
+function save() {
+  let entry = count + " - "
+  saveEl.textContent += entry
 }
